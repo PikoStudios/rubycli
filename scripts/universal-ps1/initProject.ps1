b32b25a30b8a2a31cc469c54e92688d65346a330
OkiStuff <probro1802@gmail.com>
2022-04-13 15:01:09 -0400

    finish initProject.ps1

--- scripts/universal-ps1/initProject.ps1
+++ scripts/universal-ps1/initProject.ps1
@@ -1,3 +1,6 @@
+# Version URI
+$verUri = "https://raw.githubusercontent.com/PikoStudios/rubycli/main/.version"
+
 Set-Location ../..
 
 Write-Output "==> STAND BY <== :: Creating 'gembuild' folder"
@@ -7,6 +10,16 @@ Write-Output "==> STAND BY <== :: Checking version"
 $version = Get-Content -Path .version
 
 Write-Output "==> STAND BY <== :: Checking for updates"
-Invoke-WebRequest -Uri 
+$verRequest = Invoke-RestMethod -Uri $verUri
+
+if ($verRequest -eq $version)
+{
+    Write-Output "* Up to date *"
+}
+else
+{
+    $formattedString = "* New version avaliable... Version {0}" -f $verRequest
+    Write-Output $formattedString
+}
 
 Write-Output "==> INFO <== :: Completed initialization"
