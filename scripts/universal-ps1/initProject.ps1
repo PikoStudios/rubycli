b9d2c1f5e6294dfb589ba9bcd5c56062440e557b
OkiStuff <probro1802@gmail.com>
2022-04-13 14:44:34 -0400

    removal of .verification and adding of .version

--- scripts/universal-ps1/initProject.ps1
+++ scripts/universal-ps1/initProject.ps1
@@ -1,7 +1,12 @@
-# Variables
-$verify_urls = @()
-
 Set-Location ../..
-Write-Output "==> STAND BY <== :: Creating 'gembuild' folder"
 
+Write-Output "==> STAND BY <== :: Creating 'gembuild' folder"
 New-Item -Path gembuild -ItemType Directory
+
+Write-Output "==> STAND BY <== :: Checking version"
+$version = Get-Content -Path .version
+
+Write-Output "==> STAND BY <== :: Checking for updates"
+Invoke-WebRequest -Uri 
+
+Write-Output "==> INFO <== :: Completed initialization"
